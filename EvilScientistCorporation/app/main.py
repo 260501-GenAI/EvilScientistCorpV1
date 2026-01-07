342fd9efbc865fbbe2b67f20078b26cb0cde05a4
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-01-07 11:32:21 -0500

    PUT and exception handling

--- EvilScientistCorporation/app/main.py
+++ EvilScientistCorporation/app/main.py
@@ -1,9 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
+from starlette.requests import Request
+from starlette.responses import JSONResponse
 
 from app.routers import users
 
 # Set up FastAPI. We'll use this "app" variable to do FastAPI stuff below
 app = FastAPI()
+
+# Global custom Exception Handler
+@app.exception_handler(HTTPException)
+async def custom_http_exception_handler(request:Request, exception:HTTPException):
+    return JSONResponse(
+        status_code=exception.status_code,
+        content={"message":exception.detail}
+    )
 
 # Import routers here
 app.include_router(users.router)
